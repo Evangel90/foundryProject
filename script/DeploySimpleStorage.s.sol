e114a56ce0f742595b05c34cb1ba74e1e8c836cc
Evangel90 <oladipoevangel90@gmail.com>
2026-01-24 23:48:18 +0100

    used forge fmt

--- script/DeploySimpleStorage.s.sol
+++ script/DeploySimpleStorage.s.sol
@@ -4,8 +4,8 @@ pragma solidity 0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {SimpleStorage} from "../src/SimpleStorage.sol";
 
-contract DeployCounter is Script{
-    function run() external returns (SimpleStorage){
+contract DeployCounter is Script {
+    function run() external returns (SimpleStorage) {
         vm.startBroadcast();
 
         SimpleStorage simpleStorage = new SimpleStorage();
